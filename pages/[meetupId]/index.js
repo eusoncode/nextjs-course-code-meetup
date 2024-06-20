f8698cccdbdde5593008edc3bf2691ce321f0c87
eusoncode <git@github.com:eusoncode/Kata.git>
2024-06-20 00:56:52 -0600

    Add Meetup details and css module

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,5 +1,24 @@
-export default  function Details() {
+import MeetupDetails from '../../components/meetups/MeetupDetails';
+
+
+// export default function MeetupDetail() {
+//   return (
+//     <MeetupDetails
+//       image='https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Restaurant_in_The_Mus%C3%A9e_d%27Orsay.jpg/640px-Restaurant_in_The_Mus%C3%A9e_d%27Orsay.jpg'
+//       title='First Meetup'
+//       address='Some Street 5, Some city'
+//       description='This is a first meetup'
+//     />
+//   );
+// };
+
+export default function MeetupDetail() {
   return (
-    <h1>Details page</h1>
-  )
+    <MeetupDetails
+      image='https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Restaurant_in_The_Mus%C3%A9e_d%27Orsay.jpg/640px-Restaurant_in_The_Mus%C3%A9e_d%27Orsay.jpg'
+      title='First Meetup'
+      address='Some Street 5, Some City'
+      description='This is a first meetup'
+    />
+  );
 };
